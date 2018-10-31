1c4e3671947e9a5f2a8119f0bb43de647ab05f75
vladpaun2811 <44651761+vladpaun2811@users.noreply.github.com>
2018-10-31 20:57:11 +0200

    Add files via upload

---
+++ code-in_1.py
@@ -0,0 +1,9 @@
+str1="GCI is great"
+str2="What's your name?"
+
+i =0
+while i<11:
+    print(str1)
+    i= i +1
+a= input(str2)
+print("hello "+ a +", please to meet you!Did you know that your name backwards is " + a[::-1] )
